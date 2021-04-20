465c94d4442499cc063b31c6b6dbdd4c7b544a49
Kristijan Cetina <kristijan.cetina@gmail.com>
2021-04-20 17:19:15 +0200

    add jsonify

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    response = jsonify("Hello, World!")
+    return response
