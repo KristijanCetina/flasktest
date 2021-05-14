41452b50751fe4f505f1b03039a32f1d76c62eb4
Kristijan Cetina <kristijan.cetina@gmail.com>
2021-05-14 21:21:54 +0200

    json data mockup

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-from flask import jsonify
+from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
@@ -7,10 +6,20 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-        response = "Hello, I got a GET"
+        response = "I got a GET"
     elif request.method == 'POST':
         response = "Hello, POST method"
     else:
         # iako nema smisla jer su dozovljene samo GET i POST metode ali neka se nađe  za sada :-)
         response = "Something else I don't understand"
     return jsonify(response)
+
+
+@app.route("/sensors", methods=['GET'])
+def returnSensorsData():
+    return send_from_directory("./", "sensors.json")
+
+
+@app.route("/experiments", methods=['GET'])
+def returnExperimentsData():
+    return send_from_directory("./", "experiments.json")
